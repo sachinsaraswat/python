6e8eac33fc590bade35ce6fb2f6dd052ea7cd34d
Sachin Saraswat <sswt.git@gmail.com>
2022-01-16 13:55:59 -0500

    Lists

---
+++ basic/Lists.py
@@ -0,0 +1,22 @@
+# List of variables of different types
+
+FruitAttrList = ["Apple", "Gala", "Redish-Golden", 19.99, 5]
+
+print (FruitAttrList)
+
+print(FruitAttrList[0])
+print(FruitAttrList[1])
+
+# update
+FruitAttrList[1] = "Fuji"
+
+print (FruitAttrList)
+
+
+# Delete 
+
+del FruitAttrList[1]
+
+print (FruitAttrList)
+
+print (len(FruitAttrList))
