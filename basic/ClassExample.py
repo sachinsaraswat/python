b47b36bccf3e696b1e7af9069090b01b8d0a2eb1
Sachin Saraswat <sswt.git@gmail.com>
2022-01-17 12:41:34 -0500

    Constructor parameters

--- basic/ClassExample.py
+++ basic/ClassExample.py
@@ -4,7 +4,9 @@ class Vehicle:
     def __init__(self):
         print ("Init")
         
-    
+    def __init__(self, launchSpeed =0):
+        self.speed = launchSpeed 
+        
     def accelerate(self, accelerateAmount):
         self.speed += accelerateAmount
         
@@ -16,3 +18,11 @@ Camry  = Vehicle()
 print ("Camry Speed now :: ", Camry.speed)
 Camry.accelerate(10)
 print ("Camry Speed after Aceleration is :: " , Camry.speed)
+
+rocketCar = Vehicle(1000)
+
+print("Tesla Rocket Car is moving at ::" , rocketCar.speed)
+
+
+del rocketCar
+del Camry
