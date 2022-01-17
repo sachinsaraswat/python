7b671da44f7fb351fc2dab811e7dc300ab8601d1
Sachin Saraswat <sswt.git@gmail.com>
2022-01-16 22:01:45 -0500

    userFunction

---
+++ basic/UserFunction.py
@@ -0,0 +1,13 @@
+def simplePrint( msg):  #Function definition
+    print("Func::" + msg)
+    
+
+def add( x , y):
+    return (x +y)
+
+
+#Main 
+
+simplePrint("Hello World!!")
+
+print ( "Total is %d" %(add(10, 20)))
