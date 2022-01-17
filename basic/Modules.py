079e9f8ff4baab4b00efbddf204f8217ac05f9d1
Sachin Saraswat <sswt.git@gmail.com>
2022-01-17 10:50:54 -0500

    Modules

---
+++ basic/Modules.py
@@ -0,0 +1,15 @@
+import random
+import math
+
+random.seed(10) # input to Random to start with 
+
+print(random.random())
+
+numList = ( 1, 2, 3 ,4 ,5 )
+
+print (math.fsum(numList)) # sum of the list
+
+print (math.ceil(12.01)) # round up to next integer
+
+print (math.floor(12.89)) # round down to interger just below this  value, aka truncate
+
